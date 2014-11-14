ba1d435b4ef6cf1312bae87857bc12241b6b312c
jsdima <dmitry.genov@gmail.com>
2014-11-14 15:44:12 +0300

    Add steps emulation like a wizard
    in real world may better use something like accordion componentt

--- app/scripts/controllers/self-registration.js
+++ app/scripts/controllers/self-registration.js
@@ -13,11 +13,18 @@ var questionType = {
   number:"number",
   entity:"entity"
 };
+var PAGES = {
+  Answer:"Answer",
+  Finish:"Finish",
+  FollowUP:"FollowUP"
+};
 angular.module('jsbandApp')
   .controller('SelfRegistrationCtrl', function ($scope) {
-//    @TODO How to proceed with not Existing translation
+    $scope.PAGES = PAGES;
+    $scope.currentPage = PAGES.Answer;
+//    TODO How to proceed with not Existing translation
     $scope.language='de';
-    //@TODO What abount icons?
+    //TODO What abount icons?
     $scope.availableLanguages={
       en:"English",
       de:"Deutsch",
@@ -95,10 +102,28 @@ angular.module('jsbandApp')
       }
     };
     $scope.finish = function (){
-      // some validation here
-      console.log($scope.chapters);
+
+
     };
     $scope.translate = function (mlstring){
       return mlstring[$scope.language] || mlstring[$scope.systemLanguage] || "no translation";
+    };
+    $scope.saveAndNext = function () {
+      // some validation here
+      console.log($scope.chapters);
+      $scope.currentPage = PAGES.Finish;
+    };
+    $scope.gotoAnswer = function () {
+      // some validation here
+      $scope.currentPage = PAGES.Answer;
+    };
+    $scope.finishSelfRegistration = function () {
+      // save answer on server & process result
+      $scope.currentPage = PAGES.FollowUP;
+    };
+    $scope.closeSelfRegistration = function () {
+      // send redirect to server login
+      // for emulation do it via
+      window.location = '/';
     };
   });
